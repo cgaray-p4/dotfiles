fa08352b254330715355bf4e807271ca63011c8b
Christopher Garay <chris@garay.io>
2024-03-11 20:44:02 -0400

    clean up ts config

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -2,57 +2,16 @@ return {
   -- add the rose pine theme
   { "rose-pine/neovim" },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load the rose-pine theme
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "rose-pine",
     },
   },
+
   {
     "nvim-lualine/lualine.nvim",
-  },
-
-  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
-
-  -- R support
-  {
-    "R-nvim/R.nvim",
-    config = function()
-      -- Create a table with the options to be passed to setup()
-      local opts = {
-        R_args = { "--quiet", "--no-save" },
-        hook = {
-          after_config = function()
-            -- This function will be called at the FileType event
-            -- of files supported by R.nvim. This is an
-            -- opportunity to create mappings local to buffers.
-            if vim.o.syntax ~= "rbrowser" then
-              vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
-              vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
-            end
-          end,
-        },
-        min_editor_width = 72,
-        rconsole_width = 78,
-        disable_cmds = {
-          "RClearConsole",
-          "RCustomStart",
-          "RSPlot",
-          "RSaveClose",
-        },
-      }
-      -- Check if the environment variable "R_AUTO_START" exists.
-      -- If using fish shell, you could put in your config.fish:
-      -- alias r "R_AUTO_START=true nvim"
-      if vim.env.R_AUTO_START == "true" then
-        opts.auto_start = 1
-        opts.objbr_auto_start = true
-      end
-      require("r").setup(opts)
-    end,
-    lazy = false,
   },
 
   -- add any tools you want to have installed below
@@ -68,8 +27,5 @@ return {
         "markdownlint",
       },
     },
-  },
-  {
-    "mattn/emmet-vim",
   },
 }
